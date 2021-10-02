2cd830498d7e1b7a90664ec955914a45b52572be
Wajahatulla Sharif <wajahathulla132@gmail.com>
2021-10-02 21:18:44 +0530

    added fasteners products

---
+++ js/data.js
@@ -0,0 +1,98 @@
+var productData = {
+    fasteners: {
+        bolts: {
+            category: "bolts",
+            products: [
+                {
+                    url: "image/fasteners/bolts/cap head or Carriage bolt.jpeg",
+                    name: "Cap head or Catridge bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/concrete-anchor-bolt.jpg",
+                    name: "Concrete-anchor bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/counter sunk head bolt.jpeg",
+                    name: "Counter sunk head bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/Drop Ancher bolt.jpeg",
+                    name: "Drop Ancher bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/flange-bolts.jpg",
+                    name: "Flange bolts",
+                },
+                {
+                    url: "image/fasteners/bolts/Foundation-bolt.jpg",
+                    name: "Foundation bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/Foundation-bolt.jpg",
+                    name: "Foundation bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/hexagonal bolt.jpg",
+                    name: "Hexagonal bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/Knob Bolt.jpeg",
+                    name: "Knob bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/patta-bolt.jpg",
+                    name: "Patta bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/rawl-bolt.jpg",
+                    name: "Rawl bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/roofing-bolts.jpg",
+                    name: "Roofing bolts",
+                },
+                {
+                    url: "image/fasteners/bolts/Square head bolt.jpg",
+                    name: "Square head bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/T-bolt.jpg",
+                    name: "T-bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/Wedge Ancher or Through bolt.jpg",
+                    name: "Wedge Ancher or Through bolt",
+                },
+                {
+                    url: "image/fasteners/bolts/Wing bolt.jpg",
+                    name: "Wing bolt",
+                },
+            ],
+        },
+        clips_and_pins:{
+            category: "clips_and_pins",
+            products: [
+                {
+                    url: "image/fasteners/clips and pins/blind-rivets.jpg",
+                    name: "Blind rivets",
+                },
+                {
+                    url: "image/fasteners/clips and pins/cotter-pin.jpg",
+                    name: "Cotter pin",
+                },
+                {
+                    url: "image/fasteners/clips and pins/dowel-pins.jpg",
+                    name: "Dowel pins",
+                },
+                {
+                    url: "image/fasteners/clips and pins/internal-and-external-circlips.jpg",
+                    name: "Internal and external circle clips",
+                },
+                {
+                    url: "image/fasteners/clips and pins/R type safety pin.png",
+                    name: "R type safety pin",
+                },
+            ],
+        },
+    },
+};

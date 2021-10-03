3d5fd400c8102f45631b58277777ef1f690a638a
Wajahatulla Sharif <wajahathulla132@gmail.com>
2021-10-03 18:56:37 +0530

    fasteners page almost finished

--- js/data.js
+++ js/data.js
@@ -1,98 +1,331 @@
 var productData = {
     fasteners: {
-        bolts: {
-            category: "bolts",
-            products: [
-                {
-                    url: "image/fasteners/bolts/cap head or Carriage bolt.jpeg",
-                    name: "Cap head or Catridge bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/concrete-anchor-bolt.jpg",
-                    name: "Concrete-anchor bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/counter sunk head bolt.jpeg",
-                    name: "Counter sunk head bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/Drop Ancher bolt.jpeg",
-                    name: "Drop Ancher bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/flange-bolts.jpg",
-                    name: "Flange bolts",
-                },
-                {
-                    url: "image/fasteners/bolts/Foundation-bolt.jpg",
-                    name: "Foundation bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/Foundation-bolt.jpg",
-                    name: "Foundation bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/hexagonal bolt.jpg",
-                    name: "Hexagonal bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/Knob Bolt.jpeg",
-                    name: "Knob bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/patta-bolt.jpg",
-                    name: "Patta bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/rawl-bolt.jpg",
-                    name: "Rawl bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/roofing-bolts.jpg",
-                    name: "Roofing bolts",
-                },
-                {
-                    url: "image/fasteners/bolts/Square head bolt.jpg",
-                    name: "Square head bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/T-bolt.jpg",
-                    name: "T-bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/Wedge Ancher or Through bolt.jpg",
-                    name: "Wedge Ancher or Through bolt",
-                },
-                {
-                    url: "image/fasteners/bolts/Wing bolt.jpg",
-                    name: "Wing bolt",
-                },
-            ],
-        },
-        clips_and_pins:{
-            category: "clips_and_pins",
-            products: [
-                {
-                    url: "image/fasteners/clips and pins/blind-rivets.jpg",
-                    name: "Blind rivets",
-                },
-                {
-                    url: "image/fasteners/clips and pins/cotter-pin.jpg",
-                    name: "Cotter pin",
-                },
-                {
-                    url: "image/fasteners/clips and pins/dowel-pins.jpg",
-                    name: "Dowel pins",
-                },
-                {
-                    url: "image/fasteners/clips and pins/internal-and-external-circlips.jpg",
-                    name: "Internal and external circle clips",
-                },
-                {
-                    url: "image/fasteners/clips and pins/R type safety pin.png",
-                    name: "R type safety pin",
-                },
-            ],
-        },
+        products: [
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Anti theft or shear nut.jpeg",
+                name: "Anti theft or shear nut",
+            },
+            {
+                category: "clips_and_pins",
+                url: "image/fasteners/clips and pins/blind-rivets.jpg",
+                name: "Blind rivets",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/button  head screw.jpeg",
+                name: "Button head screw",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/cap nut.webp",
+                name: "Cap nut",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/cap head or Carriage bolt.jpeg",
+                name: "Cap head or Catridge bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/castel nut.jpeg",
+                name: "Castel nut",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Conical Washer.jpg",
+                name: "Conical Washer",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/coupling nut.jpeg",
+                name: "Coupling nut",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/counter sunk machine screw.jpeg",
+                name: "Countersunk machine screw",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/coutersunk-washer.webp",
+                name: "Coutersunk washer",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Crescent-spring-washer.webp",
+                name: "Crescent spring washer",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/c-washer.jpg",
+                name: "C-washer",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/concrete-anchor-bolt.jpg",
+                name: "Concrete-anchor bolt",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/counter sunk head bolt.jpeg",
+                name: "Counter sunk head bolt",
+            },
+            {
+                category: "clips_and_pins",
+                url: "image/fasteners/clips and pins/cotter-pin.jpg",
+                name: "Cotter pin",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Deck screw.jpeg",
+                name: "Deck screw",
+            },
+            {
+                category: "clips_and_pins",
+                url: "image/fasteners/clips and pins/dowel-pins.jpg",
+                name: "Dowel pins",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Dome-spring-washer.webp",
+                name: "Dome-spring washer",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Drop Ancher bolt.jpeg",
+                name: "Drop Ancher bolt",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Drywall screw.webp",
+                name: "Drywall screw",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Eye bolt.webp",
+                name: "Eye bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Eye nut.jpeg",
+                name: "Eye nut",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/external-tooth-washer.webp",
+                name: "External-tooth washer",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/fender-washer.webp",
+                name: "Fender-washer",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Finger-spring-washer.webp",
+                name: "Finger-spring washer",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/flange-bolts.jpg",
+                name: "Flange bolts",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Flange serrated nut.jpg",
+                name: "Flange serrated nut",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/flat-washer.webp",
+                name: "Flat washer",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Foundation-bolt.jpg",
+                name: "Foundation bolt",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Hanger bolt.jpg",
+                name: "Hanger bolt",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/hexagonal bolt.jpg",
+                name: "Hexagonal bolt",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Hex leg screw.jpeg",
+                name: "Hex leg screw",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Hexagonal nut.webp",
+                name: "Hexagonal nut",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Hexagonal slotted nut.jpeg",
+                name: "Hexagonal slotted nut",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Hexagonal weld nut.jpeg",
+                name: "Hexagonal weld nut",
+            },
+            {
+                category: "clips_and_pins",
+                url: "image/fasteners/clips and pins/internal-and-external-circlips.jpg",
+                name: "Internal and external circle clips",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/internal-tooth-washer.webp",
+                name: "Internal-tooth washer",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/keps-K lock nuts.jpeg",
+                name: "Keps-K lock nuts",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Knob Bolt.jpeg",
+                name: "Knob bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Knurled thumb nut.jpeg",
+                name: "Knurled thumb nut",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Masonry screw.jpeg",
+                name: "Masonry screw",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Nylon inserted lock nut.jpeg",
+                name: "Nylon inserted lock nut",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/patta-bolt.jpg",
+                name: "Patta bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Pipe nut.jpeg",
+                name: "Pipe nut",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/rawl-bolt.jpg",
+                name: "Rawl bolt",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/roofing-bolts.jpg",
+                name: "Roofing bolts",
+            },
+            {
+                category: "clips_and_pins",
+                url: "image/fasteners/clips and pins/R type safety pin.png",
+                name: "R type safety pin",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Set Screw.jpeg",
+                name: "Set screw",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Sheet metal screw.jpeg",
+                name: "Sheet metal screw",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/shoulder-washer.webp",
+                name: "Shoulder washer",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Slef drilling screw.jpeg",
+                name: "Slef drilling screw",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Socket head screw.jpeg",
+                name: "Socket head screw",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Split-lock-washer.webp",
+                name: "Split-lock washer",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Square head bolt.jpg",
+                name: "Square head bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Square nut.jpeg",
+                name: "Square nut",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/square weld nut.jpeg",
+                name: "Square weld nut",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/T-bolt.jpg",
+                name: "T-bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/T-nut.jpg",
+                name: "T-nut",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Thread cutting screw.jpg",
+                name: "Thread cutting screw",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Torque-washer.webp",
+                name: "Torque washer",
+            },
+            {
+                category: "washers",
+                url: "image/fasteners/washers/Wave-spring-washer.webp",
+                name: "Wave-spring washer",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Wedge Ancher or Through bolt.jpg",
+                name: "Wedge Ancher or Through bolt",
+            },
+            {
+                category: "bolts",
+                url: "image/fasteners/bolts/Wing bolt.jpg",
+                name: "Wing bolt",
+            },
+            {
+                category: "nuts",
+                url: "image/fasteners/nuts/Wing nut.jpeg",
+                name: "Wing nut",
+            },
+            {
+                category: "screws",
+                url: "image/fasteners/screws/Wood screw.jpeg",
+                name: "Wood screw",
+            },
+        ],
     },
 };
